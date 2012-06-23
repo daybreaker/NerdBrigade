8e4b5002ef90feb7843df65f2eed3e1daed7c377
Brad Huber <daybreaker@gmail.com>
2012-06-22 20:14:07 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NerdBrigade::Application.config.secret_token = 'ea9d83fdfb1af83b046054467182f06d95c634cd2e739e6d683d9cc84643d25fb0369f6f653f8c50863cadb7571caad3d03865c717ec87972ba18fe980cec2e4'
