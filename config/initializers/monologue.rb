06a918551b5076bde1b0d36c264500c983925f15
Brad Huber <daybreaker@gmail.com>
2012-06-23 01:01:15 -0500

    removing ssl for now

--- config/initializers/monologue.rb
+++ config/initializers/monologue.rb
@@ -11,7 +11,7 @@ Monologue.twitter_username     = "daybreaker"
 Monologue.twitter_locale       = "en" # This is the locale for twitter. "en" for english or "fr" for french. There is much more!
 Monologue.facebook_like_locale = "en_US" # same, but for facebook. French is "fr_CA"
 Monologue.google_plusone_locale = "en" # same for Google +
-Monologue.admin_force_ssl       = true # that can be set to false if you don't have SSL, but it is not recommended
+Monologue.admin_force_ssl       = false # that can be set to false if you don't have SSL, but it is not recommended
 Monologue.posts_per_page      = 5 # OPTIONAL: number of posts per page on main page. It defaults to 10 currently
 Monologue.google_analytics_id  = "" # OPTIONAL: Your Google Analytics code should be here if you have one.
 
