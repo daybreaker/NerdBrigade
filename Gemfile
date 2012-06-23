67390e54bc2d579bd34a5dba528beba91c025dbe
Brad Huber <daybreaker@gmail.com>
2012-06-22 21:27:01 -0500

    Added Monologue

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ end
 gem 'haml'
 gem 'jquery-rails'
 
+gem "monologue"
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
---
+++ config/initializers/monologue.rb
@@ -0,0 +1,19 @@
+# config/initializers/monologue.rb
+
+# Version 0.1.0+
+Monologue.site_name            = "Nerd Brigade"
+Monologue.site_subtitle        = "Bringing you news from the Nerdery"
+Monologue.site_url             = "http://nerdbrigade.org"
+Monologue.disqus_shortname     = "daybreaker" # register on disqus.com if you don't have one
+Monologue.meta_description     = "A collection of nerdy things"
+Monologue.meta_keyword         = "nerd brigade, daybreaker, nerds"
+Monologue.twitter_username     = "daybreaker"
+Monologue.twitter_locale       = "en" # This is the locale for twitter. "en" for english or "fr" for french. There is much more!
+Monologue.facebook_like_locale = "en_US" # same, but for facebook. French is "fr_CA"
+Monologue.google_plusone_locale = "en" # same for Google +
+Monologue.admin_force_ssl       = true # that can be set to false if you don't have SSL, but it is not recommended
+Monologue.posts_per_page      = 5 # OPTIONAL: number of posts per page on main page. It defaults to 10 currently
+Monologue.google_analytics_id  = "" # OPTIONAL: Your Google Analytics code should be here if you have one.
+
+# Version 0.1.1+
+Monologue.layout               = "layouts/monologue/application" # set the layout you want to use if you want to use your main_app layout
